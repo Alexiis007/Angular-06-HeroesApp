258300728abfd28be587106cd6c3cf4fd411162a
Alexiis007 <christianalexis0918@gmail.com>
2024-04-16 14:35:24 -0600

    Finalizacion Seccion 14

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+
+@NgModule({
+  declarations: [
+    LayoutPagesComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
